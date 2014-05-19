48ee90d64f499325e3a48d9176b86a087356589e
Marc Bluemner <Marc.Bluemner@mspag.com>
2014-05-19 22:40:00 +0200

    update barcode4j

--- app/controllers/Barcodes.scala
+++ app/controllers/Barcodes.scala
@@ -20,4 +20,22 @@ object Barcodes extends Controller {
         BadRequest("Couldnt generate bar code. Error: " + e.getMessage)
     }
   }
+
+  def ean13BarCode(ean: Long, MimeType: String): Array[Byte] = {
+    import java.io.ByteArrayOutputStream
+    import java.awt.image.BufferedImage
+    import org.krysalis.barcode4j.ouput.bitmap.BitmapCanvasProvider
+    import org.krysalis.barcode4j.impl.upcean.EAN13Bean
+
+    val outpu: ByteArrayOutputStream = new ByteArrayOutputStream
+    val canvas: BitmapCanvasProvider = 
+      new BitmapCanvasProvider(output, mimeType, ImageResolution,
+    BufferedImage.TYPE_BYTE_BINARY, false, 0)
+
+  val barcode = new EAN13Bean()
+    barcode.generateBarcode(canvas, String valueOf ean)
+  canvas.finish
+
+  output.toByteArray
+  }
 }
